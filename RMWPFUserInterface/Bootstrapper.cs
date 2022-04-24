487aff3aa58bb4104060e94aed30015770bedcf4
David Pilarski <52269155+mooncorn@users.noreply.github.com>
2022-04-23 20:01:39 -0400

    Create Login Form

--- RMWPFUserInterface/Bootstrapper.cs
+++ RMWPFUserInterface/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using RMWPFUserInterface.Helpers;
 using RMWPFUserInterface.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace RMWPFUserInterface
 {
@@ -16,6 +18,11 @@ namespace RMWPFUserInterface
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
---
+++ RMWPFUserInterface/ViewModels/LoginViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMWPFUserInterface.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get { return _username; }
+            set 
+            {
+                _username = value;
+                NotifyOfPropertyChange(() => Username);
+                NotifyOfPropertyChange(() => CanLogin);
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set 
+            { 
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogin);
+            }
+        }
+
+        public bool CanLogin
+        {
+            get
+            {
+                return !String.IsNullOrEmpty(Username) && !String.IsNullOrEmpty(Password);
+            }
+        }
+
+        public void Login()
+        {
+            
+        }
+
+    }
+}
