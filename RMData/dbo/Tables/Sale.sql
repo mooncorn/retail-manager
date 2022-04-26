300c65520e7431a125aa8ad6a20def1a5bb52b54
David Pilarski <52269155+mooncorn@users.noreply.github.com>
2022-04-26 13:37:55 -0400

    Update EF Database Name

--- RMData/dbo/Tables/Sale.sql
+++ RMData/dbo/Tables/Sale.sql
@@ -2,8 +2,9 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [SellerId] NVARCHAR(128) NOT NULL, 
-    [SaleDate] DATETIME2 NOT NULL , 
+    [SaleDate] DATETIME2 NOT NULL, 
     [SubTotal] MONEY NOT NULL, 
     [Tax] MONEY NOT NULL, 
-    [Total] MONEY NOT NULL
+    [Total] MONEY NOT NULL, 
+    CONSTRAINT [FK_Sale_ToUser] FOREIGN KEY (SellerId) REFERENCES [User](Id)
 )
