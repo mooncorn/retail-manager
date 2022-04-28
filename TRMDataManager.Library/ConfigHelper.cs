b2444db35c6446eeea5032cfd16e26ba9bcce40f
David Pilarski <52269155+mooncorn@users.noreply.github.com>
2022-04-28 18:59:03 -0400

    Add ConfigHelper To API

---
+++ TRMDataManager.Library/ConfigHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRMDataManager.Library
+{
+    public class ConfigHelper
+    {
+        public static decimal TaxRate
+        {
+            get
+            {
+                try
+                {
+                    return Convert.ToDecimal(ConfigurationManager.AppSettings.Get("taxRate"));
+                }
+                catch (FormatException ex)
+                {
+                    throw new ConfigurationErrorsException($"The tax rate is not set up properly: {ex.Message}");
+                }
+            }
+        }
+    }
+}
