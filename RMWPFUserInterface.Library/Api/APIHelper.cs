f6e18257d2db249f86c1dd3750cd0fd7c77b556f
David Pilarski <52269155+mooncorn@users.noreply.github.com>
2022-05-05 11:53:34 -0400

    WPF Library Upgrade

--- RMWPFUserInterface.Library/Api/APIHelper.cs
+++ RMWPFUserInterface.Library/Api/APIHelper.cs
@@ -5,8 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Configuration;
 using RMWPFUserInterface.Library.Models;
+using System.Configuration;
 
 namespace RMWPFUserInterface.Library.Api
 {
